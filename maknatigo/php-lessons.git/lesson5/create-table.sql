cbd46a031a6a51aebe369a71d341a530d2ec5665
maknatigo <maknatigo@yandex.ru>
2019-10-22 13:25:07 +0300

    delete

--- maknatigo/php-lessons.git/lesson5/create-table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE pictures (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  path VARCHAR(255) NOT NULL,
-  size INT NOT NULL,
-  name VARCHAR(64),
-  view INT DEFAULT NULL,
-  click INT DEFAULT NULL,
-  description TEXT
-);
