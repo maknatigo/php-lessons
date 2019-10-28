fcb409800626842380ebaaf1eaebcd876e1d6cf0
maknatigo <maknatigo@yandex.ru>
2019-10-28 16:03:25 +0300

    6 задание

---
+++ lesson6/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html><html><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'>
+<!--[if lt IE 9]><meta http-equiv='X-UA-Compatible' content='IE=edge'><![endif]-->
+<style>html{height:50%;font-family:sans-serif;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;font-size:10px;-webkit-tap-highlight-color:transparent}*,:after,:before{-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box}body{height:100%;margin:0;font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;font-size:14px;line-height:1.42857143;color:#333;background-color:#fff}button,h3,input{font-family:inherit}h3{font-weight:500;line-height:1.1;color:inherit;margin-top:0;margin-bottom:20px;font-size:24px}.authform{width:300px;background-color:#e1e5ec;padding:25px 27px;margin: 0 auto;-moz-border-radius:4px;-webkit-border-radius:4px;border-radius:4px}</style><title>Добро пожаловать!</title></head>
+<body style='text-align: center'>
+
+<h1>Урок №6</h1>
+<ul>
+<li><a href='calculator.php'>Калькулятор</a></li>
+<li><a href='comments/public/gallery.php'>Отзывы</a></li>
+<li><a href='shop/public/index.php'>Каталог</a></li>
+</ul>
+</body>
+</html>
